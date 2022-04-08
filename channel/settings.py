9b0229f253febdbf1d82f8eb2748176796e99025
bradone-etole <omwitsabradone@gmail.com>
2022-04-08 20:24:26 +0300

    settings

--- channel/settings.py
+++ channel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =  os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['192.168.43.56', '192.168.43.88']
+ALLOWED_HOSTS = ['group-channel.herokuapp.com', 'online-channel.herokuapp.com']
 
 # Application definition
 
